894101f00d5cf6f8fcf447aba4ce4e167bcbfec1
Ubuntu <azureuser@arcelor-simran.cxmah5ck1ofehiiviq4bb0cjsf.dx.internal.cloudapp.net>
2026-04-10 09:45:05 +0000

    Added Innocent Developers

--- assessment.java
+++ assessment.java
@@ -1,6 +1,6 @@
 public class assessment {
 	public static void main(String args[]){
-		System.out.print("Hello World");
+		System.out.print("Hello Innocent Developers!");
 		System.out.print("DevOps Assessment");
 	}
 }
